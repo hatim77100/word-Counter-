00e3c472a5a855ecd0d09f708b8d0f57421a4a01
hatim77100 <najoua-hatim@hotmail.fr>
2024-10-01 17:11:28 +0200

    initial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <header className="footer">
+      <small>&copy; Copyright by Hatim. All rights reserved.</small>
+      <small>Last checked limits: 17 days ago</small>
+    </header>
+  );
+}
