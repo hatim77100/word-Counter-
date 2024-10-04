4bbfdf176c13cd768d9a590b23a4fd15ebf4d5e5
hatim77100 <najoua-hatim@hotmail.fr>
2024-10-04 18:47:15 +0200

    🔨 add  stateText and warning logic component

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,3 +1,29 @@
+import { useState } from "react";
+import Warning from "./Warning";
+
 export default function Textarea() {
-  return <textarea className="textarea" />;
+  const [text, setText] = useState("");
+  const [showWarning, setShowWarning] = useState(false);
+
+  const handleChange = (e) => {
+    let newText = e.target.value;
+    if (newText.includes("<script>")) {
+      setShowWarning(true);
+      newText = newText.replace("<script>", "");
+    }
+    setText(newText);
+    console.log(newText);
+  };
+
+  return (
+    <div className="textarea">
+      <textarea
+        value={text}
+        onChange={handleChange}
+        placeholder="Enter your text"
+        spellCheck="false"
+      />
+      {showWarning ? <Warning /> : null}
+    </div>
+  );
 }
