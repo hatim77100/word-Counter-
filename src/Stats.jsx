e4bcaf42e727f3a9fb37c778c40f195350e782ef
hatim77100 <najoua-hatim@hotmail.fr>
2024-10-05 12:41:48 +0200

    ♻️ reusable Stat component

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,3 +1,19 @@
 export default function Stats() {
-  return <section className="stats">Stats</section>;
+  return (
+    <section className="stats">
+      <Stat number={0} label="Word" />
+      <Stat number={0} label="Characters" />
+      <Stat number={280} label="Instagram" />
+      <Stat number={2200} label="Facebook" />
+    </section>
+  );
+}
+
+function Stat({ number, label }) {
+  return (
+    <section className="stat">
+      <span className="stat_number">{number}</span>
+      <h2 className="second-heading">{label}</h2>
+    </section>
+  );
 }
