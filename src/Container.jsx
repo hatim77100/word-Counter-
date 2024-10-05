609c7c974aea45e5b1320d8fd3bbf34c3fbdeceb
hatim77100 <najoua-hatim@hotmail.fr>
2024-10-05 15:21:16 +0200

    🔨 calculate charaters

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import Stats from "./Stats";
 import Textarea from "./Textarea";
 
 export default function Container() {
+  const [text, setText] = useState("");
+  const numberOfWords = text.split(/\s/).filter((word) => word !== "").length;
+  const numberOfCharacters = text.length;
+  const instagramCharactersLeft = 280 - numberOfCharacters;
+  const facebookCharactersLeft = 2200 - numberOfCharacters;
+
   return (
     <main className="container">
-      <Textarea />
-      <Stats />
+      <Textarea text={text} setText={setText} />
+      <Stats
+        numberOfWords={numberOfWords}
+        numberOfCharacters={numberOfCharacters}
+        instagramCharactersLeft={instagramCharactersLeft}
+        facebookCharactersLeft={facebookCharactersLeft}
+      />
     </main>
   );
 }
